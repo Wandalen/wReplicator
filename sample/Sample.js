879dab3566b130db547913b808c4d59325eb54e6
Wandalen <wandalen.me@gmail.com>
2019-02-21 09:43:22 +0200

    auto commit

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,7 +1,9 @@
 
-let _ = require( 'wTools' );
+let _ = require( '..' );
 
 /**/
 
-...
+let src = { a : [ 1, 2, 3 ], b : { b1 : 'text', b2 : 13 }, c : new Date }
+let got = _.replicate({ src : src });
 
+console.log( got );
