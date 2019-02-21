16b343663524a6530e258deb1cb8fc567ffe1abe
Wandalen <wandalen.me@gmail.com>
2019-02-21 09:55:52 +0200

    auto commit

--- sample/Sample.js
+++ sample/Sample.js
@@ -7,3 +7,9 @@ let src = { a : [ 1, 2, 3 ], b : { b1 : 'text', b2 : 13 }, c : new Date }
 let got = _.replicate({ src : src });
 
 console.log( got );
+
+/*
+{ a: [ 1, 2, 3 ],
+  b: { b1: 'text', b2: 13 },
+  c: 2019-02-21T07:44:11.718Z }
+*/
