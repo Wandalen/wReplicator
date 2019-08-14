b1878d19fc12636928ba18ebe47588a4e517a373
Wandalen <wandalen.me@gmail.com>
2019-08-14 08:26:34 +0300

    auto commit

--- out/debug/dwtools/abase/l4/Replicator.s
+++ out/debug/dwtools/abase/l4/Replicator.s
@@ -23,13 +23,13 @@ if( typeof module !== 'undefined' )
   let _ = require( '../../Tools.s' );
 
   _.include( 'wLooker' );
-  _.include( 'wPathFundamentals' );
 
 }
 
 let _global = _global_;
-let Self = _global_.wTools;
-let _ = _global_.wTools;
+// let Self = _global_.wTools;
+let _ = _global_.wTools
+let Parent = _.Looker;
 
 let _ArraySlice = Array.prototype.slice;
 let _FunctionBind = Function.prototype.bind;
@@ -42,13 +42,143 @@ _.assert( !!_realGlobal_ );
 // routines
 // --
 
+function dstMethodsAdd()
+{
+  let it = this;
+  // let c = it.replicateOptions;
+
+  _.assert( it.iterable !== null && it.iterable !== undefined );
+  _.assert( it.dstWriteDown === null );
+  _.assert( _.routineIs( it.dstSet ) );
+
+  // it.dstSet = dstSet;
+
+  if( !it.iterable )
+  {
+    it.dstWriteDown = function dstWriteDown( eit )
+    {
+      _.assert( 0, 'Cant write into terminal' );
+      this.dst = eit.dst;
+    }
+  }
+  else if( it.iterable === 'array-like' )
+  {
+    it.dstWriteDown = function( eit )
+    {
+      if( eit.dst !== undefined )
+      this.dst.push( eit.dst );
+    }
+  }
+  else if( it.iterable === 'map-like' )
+  {
+    it.dstWriteDown = function( eit )
+    {
+      if( eit.dst === undefined )
+      delete this.dst[ eit.key ];
+      else
+      this.dst[ eit.key ] = eit.dst;
+    }
+  }
+
+}
+
+//
+
+function dstSet()
+{
+  let it = this;
+  // let c = it.replicateOptions; // xxx
+
+  _.assert( it.dst === null );
+  _.assert( it.iterable !== null && it.iterable !== undefined );
+  _.assert( it.dstSetting );
+
+  if( !it.iterable )
+  {
+    it.dst = it.src;
+  }
+  else if( it.iterable === 'array-like' )
+  {
+    it.dst = [];
+  }
+  else if( it.iterable === 'map-like' )
+  {
+    it.dst = Object.create( null );
+  }
+
+}
+
+//
+
+function visitUp()
+{
+  let it = this;
+
+  it.visitUpBegin();
+
+  _.assert( it.visiting );
+  _.assert( it.iterable !== null && it.iterable !== undefined );
+  _.assert( _.routineIs( it.dstSet ) );
+
+  _.assert( _.routineIs( it.onUp ) );
+  let r = it.onUp.call( it, it.src, it.key, it );
+  _.assert( r === undefined );
+
+  /* */
+
+  it.dstMethodsAdd();
+
+  if( it.dstSetting )
+  it.dstSet();
+
+  /* */
+
+  it.visitUpEnd()
+
+}
+
+//
+
+function visitDown()
+{
+  let it = this;
+
+  it.visitDownBegin();
+
+  if( it.visiting )
+  if( it.onDown )
+  {
+    let r = it.onDown.call( it, it.src, it.key, it );
+    _.assert( r === undefined );
+  }
+
+  /* */
+
+  _.assert( it.iterable !== null && it.iterable !== undefined );
+
+  if( it.down && it.dstWritingDown )
+  {
+    _.assert( _.routineIs( it.down.dstWriteDown ) );
+    it.down.dstWriteDown( it );
+  }
+
+  /* */
+
+  it.visitDownEnd();
+
+  return it;
+}
+
+//
+
 function replicate_pre( routine, args )
 {
 
   let o = args[ 0 ];
   if( args.length === 2 )
   {
-    if( _.lookIterationIs( args[ 0 ] ) )
+    // if( _.lookIterationIs( args[ 0 ] ) )
+    if( Self.iterationIs( args[ 0 ] ) )
     o = { it : args[ 0 ], dst : args[ 1 ] }
     else
     o = { src : args[ 0 ], dst : args[ 1 ] }
@@ -60,27 +190,27 @@ function replicate_pre( routine, args )
   _.assert( o.onUp === null || _.routineIs( o.onUp ) );
   _.assert( o.onDown === null || _.routineIs( o.onDown ) );
 
-  o.prevReplicateOptions = null;
+  o.prevReplicateIteration = null;
   if( o.root === null )
   o.root = o.src;
 
   if( o.it )
   {
-    debugger;
     _.assert( o.src === null );
-    _.assert( _.lookIterationIs( o.it ) );
-    _.assert( _.objectIs( o.it.replicateOptions ) );
+    // _.assert( _.lookIterationIs( o.it ) );
+    _.assert( Self.iterationIs( o.it ), () => 'Expects iteration of ' + Self.constructor.name + ' but got ' + _.toStrShort( o.it ) );
+    _.assert( 0, 'not tested' );
     o.src = o.it.src;
-    o.prevReplicateOptions = o.it.replicateOptions;
+    debugger; xxx
+    o.prevReplicateIteration = o.it;
+    // o.prevReplicateOptions = o.it.iterator;
+    // o.prevReplicateOptions = o.it.replicateOptions;
   }
 
   let o2 = optionsFor( o );
-  let it = _.look.pre( _.look, [ o2 ] );
+  let it = _.look.pre( _.replicate, [ o2 ] );
 
   _.assert( o.it === it || o.it === null );
-  _.assert( it.replicateOptions === o.prevReplicateOptions || it.replicateOptions === o );
-  it.iterator.replicateOptions = o;
-  _.assert( it.replicateOptions === o );
 
   return it;
 
@@ -89,132 +219,15 @@ function replicate_pre( routine, args )
   function optionsFor( o )
   {
 
-    let o2 = Object.create( null );
-    o2.src = o.src;
-    // o2.context = o;
-    o2.onUp = up;
-    o2.onDown = down;
-    o2.Looker = Looker;
-    o2.trackingVisits = o.trackingVisits;
-    o2.it = o.it;
-    o2.recursive = o.recursive;
+    let o2 = o;
 
-    o2.iteratorExtension = o.iteratorExtension;
-    o2.iterationExtension = o.iterationExtension;
-    o2.iterationPreserve = o.iterationPreserve;
+    if( o2.Looker === null )
+    o2.Looker = Self;
 
-    o2.iteratorExtension = _.mapExtend( null, o2.iteratorExtension || {} );
-    _.assert( o2.iteratorExtension.replicateOptions === undefined );
+    _.assert( o2.replicateOptions === undefined );
     _.assert( arguments.length === 1 );
-    o2.iteratorExtension.replicateOptions = o;
 
     return o2;
-  }
-
-  /* */
-
-  function up()
-  {
-    let it = this;
-    let c = it.replicateOptions;
-
-    _.assert( it.iterable !== null && it.iterable !== undefined );
-    _.assert( it.dstSet === null );
-
-    if( c.onUp )
-    c.onUp.call( it );
-
-    dstMethods.call( it );
-
-    if( it.dstSetting )
-    it.dstSet();
-
-  }
-
-  /* */
-
-  function down()
-  {
-    let it = this;
-    let c = it.replicateOptions;
-
-    if( c.onDown )
-    c.onDown.call( it );
-
-    _.assert( it.iterable !== null && it.iterable !== undefined );
-
-    if( it.down && it.dstWritingDown )
-    {
-      _.assert( _.routineIs( it.down.dstWriteDown ) );
-      it.down.dstWriteDown( it );
-    }
-
-  }
-
-  /* - */
-
-  function dstMethods()
-  {
-    let it = this;
-    let c = it.replicateOptions;
-
-    // _.assert( it.dst === null );
-    _.assert( it.iterable !== null && it.iterable !== undefined );
-
-    it.dstSet = dstSet;
-
-    if( !it.iterable )
-    {
-      it.dstWriteDown = function dstWriteDown( eit )
-      {
-        _.assert( 0, 'Cant write into terminal' );
-        this.dst = eit.dst;
-      }
-    }
-    else if( it.iterable === 'array-like' )
-    {
-      it.dstWriteDown = function( eit )
-      {
-        if( eit.dst !== undefined )
-        this.dst.push( eit.dst );
-      }
-    }
-    else if( it.iterable === 'map-like' )
-    {
-      it.dstWriteDown = function( eit )
-      {
-        if( eit.dst === undefined )
-        delete this.dst[ eit.key ];
-        else
-        this.dst[ eit.key ] = eit.dst;
-      }
-    }
-
-  }
-
-  /* - */
-
-  function dstSet()
-  {
-    let it = this;
-    let c = it.replicateOptions;
-
-    _.assert( it.dst === null );
-    _.assert( it.iterable !== null && it.iterable !== undefined );
-
-    if( !it.iterable )
-    {
-      it.dst = it.src;
-    }
-    else if( it.iterable === 'array-like' )
-    {
-      it.dst = [];
-    }
-    else if( it.iterable === 'map-like' )
-    {
-      it.dst = Object.create( null );
-    }
-
   }
 
 }
@@ -226,28 +239,19 @@ function replicateIt_body( it )
   _.assert( arguments.length === 1, 'Expects single argument' );
   _.assert( _.lookerIs( it.Looker ) );
   _.assert( it.looker === undefined );
-  it.replicateOptions.iteration = _.look.body( it );
+  _.look.body( it );
   return it;
 }
 
-replicateIt_body.defaults =
-{
+var defaults = replicateIt_body.defaults = Object.create( _.look.defaults )
 
-  it : null,
-  root : null,
-  src : null,
-  dst :  null,
+defaults.Looker = null;
+defaults.it = null;
+defaults.root = null;
+defaults.src = null;
+defaults.dst =  null;
 
-  trackingVisits : 1,
-  iteratorExtension : null,
-  iterationExtension : null,
-  iterationPreserve : null,
-  recursive : Infinity,
-
-  onUp : null,
-  onDown : null,
-
-}
+defaults.prevReplicateIteration = null;
 
 //
 
@@ -286,7 +290,7 @@ function replicate_body( it )
   return it.dst;
 }
 
-_.routineExtend( replicate_body, replicateIt );
+_.routineExtend( replicate_body, replicateIt.body );
 
 //
 
@@ -303,18 +307,22 @@ _.routineExtend( replicate_body, replicateIt );
 let replicate = _.routineFromPreAndBody( replicate_pre, replicate_body );
 
 // --
-// looker
+// extend looker
 // --
 
-let Looker = _.mapExtend( null, _.look.defaults.Looker );
-Looker.Looker = Looker;
+let Replicator = Object.create( Parent );
+Replicator.constructor = function Replicator(){};
+Replicator.Looker = Replicator;
+Replicator.dstMethodsAdd = dstMethodsAdd;
+Replicator.dstSet = dstSet;
+Replicator.visitUp = visitUp;
+Replicator.visitDown = visitDown;
 
-Looker.Iteration = _.mapExtend( null, Looker.Iteration );
-Looker.Iteration.dst = null;
-Looker.Iteration.dstSet = null;
-Looker.Iteration.dstSetting = true;
-Looker.Iteration.dstWriteDown = null;
-Looker.Iteration.dstWritingDown = true;
+let Iteration = Replicator.Iteration = _.mapExtend( null, Replicator.Iteration );
+Iteration.dst = null;
+Iteration.dstSetting = true;
+Iteration.dstWriteDown = null;
+Iteration.dstWritingDown = true;
 
 // --
 // declare
@@ -323,18 +331,21 @@ Looker.Iteration.dstWritingDown = true;
 let Supplement =
 {
 
+  Replicator,
+
   replicateIt,
   replicate,
 
 }
 
-_.mapSupplement( Self, Supplement );
+let Self = Replicator;
+_.mapSupplement( _, Supplement );
 
 // --
 // export
 // --
 
 if( typeof module !== 'undefined' && module !== null )
-module[ 'exports' ] = Self;
+module[ 'exports' ] = _;
 
 })();
